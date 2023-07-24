ef1f3147f96b3d710e5b13b1b76e50411652895b
Edith <mkvjayanth1@gmail.com>
2023-07-25 00:54:23 +0530

    api

--- app/api/submit/route.js
+++ app/api/submit/route.js
@@ -12,11 +12,13 @@ export async function POST(request) {
     });
   }
     const res = await request.json();
-    console.log(res)
+  console.log(res)
+  const pk = process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n");
+  console.log(pk)
   const auth = new google.auth.GoogleAuth({
     credentials: {
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
     },
     scopes: [
       "https://www.googleapis.com/auth/drive",
